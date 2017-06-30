4e5a22bba9dcd6fadd5167062353a6396c56e0e7
dulajdilshan <dulaj.15@cse.mrt.ac.lk>
2017-06-30 10:51:38 +0530

    d

--- index.php
+++ index.php
@@ -7,28 +7,52 @@
     <link rel="stylesheet" type="text/css" href="font.css">
 </head>
 
-
 <body>
 
     <div class="container">
         <img src="Resources\userlogin.png">
+
         <form>
             <div class="form-input">
-                <input type="text" name="username" placeholder="Enter Username">
+                <input type="text" name="username" placeholder="Username">
+                <input type="text" name="fname" placeholder="First Name">
+                <input type="text" name="lname" placeholder="Last Name">
+                <input type="password" name="password" placeholder="Password">
             </div>
-            <div class="form-input">
-                <input type="password" name="password" placeholder="Enter Password">
-            </div>
-            <input type="submit" name="submit" value="LOGIN">
-            <div class="a">
-                <a href="">Forget password?</a>
+            <input type="submit" name="submit" value="Sign Up">
+            <div class="haveAccount">
+                <a href="">Have an account alredy?</a>
             </div>
         </form>
+
     </div>
+
 <div class="about">
-    <h3>powered by</h3>
+    <h3>Powered by</h3>
     <img src="Resources\riucsita.png">
 </div>
+
+<?php
+    $servername ='localhost';
+    $username = 'root';
+    $password = NULL;
+    $dbname = "login-test";
+
+//create connection
+$conn = new mysqli($servername,$username,$password,$dbname);
+
+//check connection
+if ($conn->connect_error){
+    die("Connection Failed: ".$conn->connect_error);
+}
+$sql = "INSERT INTO login(username,password)";
+if($conn->query($sql)==TRUE){
+    echo "New record added";
+}else{
+    echo "error: ".$sql ."<br>" .$conn->error;
+}
+$conn->close();
+?>
 
 </body>
 
